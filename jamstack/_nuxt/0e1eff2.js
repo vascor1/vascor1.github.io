066f425179b0bae774faca2a670beb5443791d52
Vasco <v.roldan@campus.fct.unl.pt>
2021-04-19 03:22:39 +0100

    [test] trying integration between Heroku and GH Pages

---
+++ jamstack/_nuxt/0e1eff2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{407:function(n,w,o){"use strict";o.r(w)}}]);
---
+++ jamstack/_nuxt/static/1618798903/products/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/products", {data:[{products:[{id:1,Title:"JAMStack Websites!",Description:"We create inovative JAMStack websites!",published_at:"2021-04-19T01:34:37.292Z",created_at:"2021-04-19T01:34:34.229Z",updated_at:"2021-04-19T01:34:37.349Z"}]}],fetch:{},mutations:void 0});
